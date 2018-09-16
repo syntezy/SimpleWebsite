475b7ef7c10b9943bd48fdd34bd3e838e2d55944
Przemysław Sypniewski <p.sypniewski@onet.pl>
2018-09-16 14:51:42 +0200

    Add simple jQuery

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.hamburger').click(function(){
+        $('.hamburger').toggleClass('active');
+    })
+    $('.hamburger').click(function(){
+        $('.menu-list').toggleClass('active');
+    })
+    $('.hamburger').click(function(){
+        $('.menu').toggleClass('active');
+    })
+})
+
